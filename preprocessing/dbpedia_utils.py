36d47555695f6a0b6119be08a6f090e20792ea2b
Andre Zanon <andrelzanon10@gmail.com>
2021-03-26 18:31:57 -0300

    Non save sparql query code and code to merge the save with the non save(get uris that the save query does not get but also misses a lot) queries dataset. After check and changes to the non save query the final dataset got to a 97% of uri retrieved from the dbpedia of the small movielens dataset

--- preprocessing/dbpedia_utils.py
+++ preprocessing/dbpedia_utils.py
@@ -3,7 +3,7 @@ from SPARQLWrapper import SPARQLWrapper, JSON
 
 def get_movie_uri_from_dbpedia(movie_name: str):
     """
-    Function the uri of a movie passed as parameter
+    Function that returns the uri of a movie passed as parameter
     :param movie_name: name of the movie
     :return: uri of the dbpedia resource of the movie_name
     """
@@ -44,6 +44,58 @@ def get_movie_uri_from_dbpedia(movie_name: str):
     results = sparql.query().convert()
     try:
         uri = results["results"]["bindings"][0].get("uri").get("value")
+    except IndexError:
+        uri = ""
+    return uri
+
+
+def get_recovery_movie_uri(movie_name: str, imdbid: str):
+    """
+    Function that returns the uri of a movie and the imdbid of the same movie passed as parameter
+    :param movie_name: name of the movie
+    :param imdbid: imdbid of the movie
+    :return: uri of the dbpedia resource of the movie_name
+    """
+
+    sparql = SPARQLWrapper("http://dbpedia.org/sparql")
+    sparql_query = """
+            PREFIX owl: <http://www.w3.org/2002/07/owl#>
+            PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+            PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+            PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+            PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+            PREFIX dc: <http://purl.org/dc/elements/1.1/>
+            PREFIX : <http://dbpedia.org/resource/>
+            PREFIX dbpedia2: <http://dbpedia.org/property/>
+            PREFIX dbpedia: <http://dbpedia.org/>       
+            PREFIX dct:	<http://purl.org/dc/terms/> 
+            PREFIX ns:<http://www.w3.org/ns/prov#>    
+            SELECT DISTINCT ?uri ?label ?abs WHERE
+            {
+                {
+                    ?uri dbo:imdbId \"""" + imdbid + """\" 
+                }
+                UNION 
+                {
+                    ?uri a dbo:Film .
+                    ?uri foaf:name ?label .
+                    FILTER (REGEX(?label, "^""" + movie_name + """$|^""" + movie_name + """", "i"))
+                }
+                UNION
+                {
+                    ?uri a dbo:Film .
+                    ?uri rdfs:comment	?abs .
+                    FILTER (CONTAINS(?abs, \"""" + movie_name + """\"))
+                }          
+            } 
+        """
+
+    sparql.setQuery(sparql_query)
+
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
+    try:
+        uri = results["results"]["bindings"][-1].get("uri").get("value")
     except IndexError:
         uri = ""
     return uri
--- preprocessing/movielens_small_utils.py
+++ preprocessing/movielens_small_utils.py
@@ -2,7 +2,10 @@ import pandas as pd
 from preprocessing import dbpedia_utils as from_dbpedia
 
 ml_small_path = "./datasets/ml-latest-small/movies.csv"
+link_ml_small_path = "./datasets/ml-latest-small/links.csv"
 uri_ml_small_path = "./generated_files/dbpedia/uri_dbpedia_movielens_small.csv"
+nan_ml_small_path = "./generated_files/dbpedia/nan_uri_dbpedia_movielens_small.csv"
+final_ml_small_path = "./generated_files/dbpedia/final_uri_dbpedia_movielens_small.csv"
 
 
 def read_movie_info():
@@ -13,12 +16,38 @@ def read_movie_info():
     return pd.read_csv(ml_small_path, usecols=['movieId', 'title']).set_index(['movieId'])
 
 
+def read_links_info():
+    """
+    Function that reads the imbd set of the movies of the small movielens dataset
+    :return: pandas DataFrame with the movieId column as index and imdbId as value
+    """
+    return pd.read_csv(link_ml_small_path, usecols=['movieId', 'imdbId']).set_index(['movieId'])
+
+
 def read_uri_info():
+    """
+    Function that reads the name of the movies of the small movielens dataset with the uri from
+    the generate_movies_uri_dbpedia_dataset function
+    :return: pandas DataFrame with the movieId column as index and title and uri as value
+    """
+    return pd.read_csv(uri_ml_small_path,).set_index(['movieId'])
+
+
+def read_nan_info():
+    """
+    Function that reads the name of the movies of the small movielens dataset with the uri from
+    the generate_recovery_uri_dbpedia function
+    :return: pandas DataFrame with the movieId column as index and title as value
+    """
+    return pd.read_csv(nan_ml_small_path).set_index(['movieId'])
+
+
+def read_final_sml_dbpedia_dataset():
     """
     Function that reads the name of the movies of the small movielens dataset
     :return: pandas DataFrame with the movieId column as index and title as value
     """
-    return pd.read_csv(uri_ml_small_path).set_index(['movieId'])
+    return pd.read_csv(final_ml_small_path).set_index(['movieId'])
 
 
 def __get_movie_strings(full_name: str):
@@ -98,3 +127,69 @@ def generate_movies_uri_dbpedia_dataset():
     full_movies = pd.concat([movies, movies_uri], axis=1)
     full_movies.to_csv(uri_ml_small_path, index=True)
     print("Finished Obtaining the URIs of the small MovieLens dataset")
+
+
+def generate_recovery_uri_dbpedia():
+    """
+    Function that generates the dataset OF NAN uris from the generate_movies_uri_dbpedia_dataset with movieId, title and
+    uri from dbpedia by invoking, for every title of movie
+    the function get_recovery_movie_uri. Because the API can cause timeout, the DBPedia API is called 10 times until
+    it successfully works, if it does not, then it goes to the next movie. A manual check to the dataset generated from
+    this method MUST be done due to the fact that the SPARQL query of the invoked function is 50% accurate only.
+    :return: A DataFrame with movieId, title and uri from dbpedia
+    """
+    movies = read_uri_info()
+    links = read_links_info()
+    movies_nan_uri = movies[movies['uri'].isna()]
+
+    print("Obtaining the NaN URIs of the small MovieLens dataset")
+    for index, row in movies_nan_uri.iterrows():
+        prev_names = __get_movie_strings(row[0])
+        names = prev_names
+        imdbid = links.loc[index, 'imdbId']
+
+        for name in prev_names:
+            s1 = name.split("-")
+            s2 = name.split(": ")
+            if len(s1) > 1:
+                names = names + s1
+            if len(s2) > 1:
+                names = names + s2
+
+        for name in names:
+            uri_name = ""
+            n = 0
+            while True:
+                try:
+                    uri_name = from_dbpedia.get_recovery_movie_uri(name, format(imdbid, '07d'))
+                except Exception as e:
+                    n = n + 1
+                    print("n:" + str(n) + " Exception: " + str(e))
+                    if n == 10:
+                        break
+
+                    continue
+                break
+
+            if uri_name != "":
+                movies_nan_uri.at[index, 'uri'] = uri_name
+                print("id: " + str(index) + " uri: " + str(uri_name) + " movie name: " + movies_nan_uri.loc[index, 'title'])
+                break
+            else:
+                print("id: " + str(index) + " URI not found for name \"" + name + "\"")
+
+    movies_nan_uri.to_csv("./generated_files/dbpedia/nan_uri_dbpedia_movielens_small.csv", index=True)
+    print("Finished Obtaining the NaN URIs of the small MovieLens dataset")
+
+
+def merge_dbpedia_dataset():
+    """
+    Merges the dataset obtained from the two sparql queries, of the generate_movies_uri_dbpedia_dataset and
+    generate_recovery_uri_dbpedia functions, with a 97% of small movie lens coverage
+    :return: final dataset with movieid, movie title and uri of dbpedia
+    """
+    uris = read_uri_info()
+    nan_uris = read_nan_info()
+
+    final = uris.fillna(nan_uris)
+    final.to_csv(final_ml_small_path, index=True)
